fc5ed84678aca0207e1fe72705489200c21edd84
Kullo Raiend <kullo.raiend@gmail.com>
2025-12-27 14:50:55 +0200

    Hetkeseis

---
+++ SharesCommon.R
@@ -0,0 +1,30 @@
+
+# Local DB
+dbName <- "shareholding" 
+host <- "localhost"
+localUserName <- "root"
+localUserPassword <- "SOMSQLRP_x2"
+
+# shares
+shareSymbols <- c(
+                  "ARC1T",
+                  "CPA1T",
+                  # "EEG1T",
+                  "EFT1T",
+                  "HAE1T",
+                  "HPR1T",
+                  "INF1T",
+                  "LHV1T",
+                  "MRK1T",
+                  "NCN1T",
+                  "PRF1T",
+                  "SFG1T",
+                  "TAL1T",
+                  "TKM1T",
+                  "TSM1T",
+                  "TVEAT"
+)
+# shareSymbols <- c("LHV1T", "NCN1T", "PRF1T", "SFG1T")
+shareSymbols <- c(
+  "ARC1T", "EFT1T", "HPR1T", "INF1T", "LHV1T", "MRK1T", "NCN1T", "PRF1T", "SFG1T", "TSM1T"
+)
